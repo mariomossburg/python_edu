dfbcc030d76208afd0b790bb6171717f2be1addd
mariano mossburg <marmossburg@gmail.com>
2024-12-16 22:23:08 -0300

    ...

---
+++ leetcode_questions/single_number.py
@@ -0,0 +1,12 @@
+
+# from typing import List
+ 
+
+# We used bit manipulation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+
+        for n in nums:
+            res = n ^ res
+        return res
